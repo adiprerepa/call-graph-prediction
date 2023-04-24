3f766680c99d33d16fcfb997e407feeedd49a888
Aditya Prerepa <adiprerepa@gmail.com>
2023-04-24 17:56:33 -0500

    initial commit

---
+++ plugin.cc
@@ -0,0 +1,6 @@
+#include "plugin.h"
+
+static RegisterContextFactory register_Scaffold(
+    CONTEXT_FACTORY(PluginContext), ROOT_FACTORY(PluginRootContext));
+
+bool PluginRootContext::onConfigure(size_t) { return true; }
