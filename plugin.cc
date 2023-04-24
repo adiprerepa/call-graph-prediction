7375beb150571a6bafa95843b2cab047d7c79d4a
Aditya Prerepa <adiprerepa@gmail.com>
2023-04-24 18:37:11 -0500

    modify plugin

--- plugin.cc
+++ plugin.cc
@@ -1,6 +1,64 @@
 #include "plugin.h"
 
 static RegisterContextFactory register_Scaffold(
-    CONTEXT_FACTORY(PluginContext), ROOT_FACTORY(PluginRootContext));
+    CONTEXT_FACTORY(PluginContext), ROOT_FACTORY(PluginRootContext), "slate_root_id");
 
 bool PluginRootContext::onConfigure(size_t) { return true; }
+
+bool PluginRootContext::onStart(size_t) {
+  LOG_TRACE("onStart");
+  return true;
+}
+
+bool PluginRootContext::onConfigure(size_t) {
+  LOG_TRACE("onConfigure");
+  proxy_set_tick_period_milliseconds(1000);
+  return true;
+}
+
+
+void PluginRootContext::onTick() { LOG_TRACE("onTick"); }
+
+void PluginContext::onCreate() { LOG_WARN(std::string("onCreate " + std::to_string(id()))); }
+
+FilterHeadersStatus PluginContext::onRequestHeaders(uint32_t, bool) {
+  LOG_DEBUG(std::string("onRequestHeaders ") + std::to_string(id()));
+  auto result = getRequestHeaderPairs();
+  auto pairs = result->pairs();
+  LOG_INFO(std::string("headers: ") + std::to_string(pairs.size()));
+  for (auto& p : pairs) {
+    LOG_INFO(std::string(p.first) + std::string(" -> ") + std::string(p.second));
+  }
+  return FilterHeadersStatus::Continue;
+}
+
+FilterHeadersStatus PluginContext::onResponseHeaders(uint32_t, bool) {
+  LOG_DEBUG(std::string("onResponseHeaders ") + std::to_string(id()));
+  auto result = getResponseHeaderPairs();
+  auto pairs = result->pairs();
+  LOG_INFO(std::string("headers: ") + std::to_string(pairs.size()));
+  for (auto& p : pairs) {
+    LOG_INFO(std::string(p.first) + std::string(" -> ") + std::string(p.second));
+  }
+  addResponseHeader("X-Wasm-custom", "SLATE optimizer");
+  return FilterHeadersStatus::Continue;
+}
+
+FilterDataStatus PluginContext::onRequestBody(size_t body_buffer_length,
+                                               bool /* end_of_stream */) {
+  auto body = getBufferBytes(WasmBufferType::HttpRequestBody, 0, body_buffer_length);
+  LOG_ERROR(std::string("onRequestBody ") + std::string(body->view()));
+  return FilterDataStatus::Continue;
+}
+
+FilterDataStatus PluginContext::onResponseBody(size_t body_buffer_length,
+                                                bool /* end_of_stream */) {
+  setBuffer(WasmBufferType::HttpResponseBody, 0, body_buffer_length, "SLATE optimizer, Hello World!\n");
+  return FilterDataStatus::Continue;
+}
+
+void PluginContext::onDone() { LOG_WARN(std::string("onDone " + std::to_string(id()))); }
+
+void PluginContext::onLog() { LOG_WARN(std::string("onLog " + std::to_string(id()))); }
+
+void PluginContext::onDelete() { LOG_WARN(std::string("onDelete " + std::to_string(id()))); }
--- plugin.h
+++ plugin.h
@@ -8,13 +8,24 @@ class PluginRootContext : public RootContext {
   explicit PluginRootContext(uint32_t id, std::string_view root_id)
       : RootContext(id, root_id) {}
 
+  bool onStart(size_t) override;
   bool onConfigure(size_t) override;
+  void onTick() override;
 };
 
 // Per-stream context.
 class PluginContext : public Context {
  public:
   explicit PluginContext(uint32_t id, RootContext* root) : Context(id, root) {}
+
+  void onCreate() override;
+  FilterHeadersStatus onRequestHeaders(uint32_t headers, bool end_of_stream) override;
+  FilterDataStatus onRequestBody(size_t body_buffer_length, bool end_of_stream) override;
+  FilterHeadersStatus onResponseHeaders(uint32_t headers, bool end_of_stream) override;
+  FilterDataStatus onResponseBody(size_t body_buffer_length, bool end_of_stream) override;
+  void onDone() override;
+  void onLog() override;
+  void onDelete() override;
 
  private:
   inline PluginRootContext* rootContext() {
