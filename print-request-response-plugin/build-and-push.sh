e384702ba248a16541df68c27797a215813b9baf
Aditya Prerepa <adiprerepa@gmail.com>
2023-05-05 17:54:53 -0500

    cleanup

--- print-request-response-plugin/build-and-push.sh
+++
@@ -1,3 +0,0 @@
-tinygo build -o wasm-out/print_slate_plugin.wasm -scheduler=none -target=wasi main.go
-docker build -t ghcr.io/adiprerepa/print-slate-plugin:latest .
-docker push ghcr.io/adiprerepa/print-slate-plugin:latest
